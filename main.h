c5874fa9a42bcd3dfa7e1145b238fbbb947e02fb
BryanField15 <6367@holbertonstudents.com>
2023-03-28 02:32:18 -0700

    first draft of printf

--- main.h
+++ main.h
@@ -3,7 +3,17 @@
 
 #include <stdlib.h>
 #include <stdarg.h>
+#include <stddef.h>
+
+typedef struct check
+{
+	char *op;
+	void(*f)(va_list args);
+} check_t;
 
 int _printf(const char *format, ...);
+int _strlen(char *s);
+int _putchar(char c);
+void print_str(char *str);
 
-#endif /* _MAIN_H */
+#endif /* _MAIN_H_ */
---
+++ printf.c
@@ -0,0 +1,57 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ *printf - produces output according to a format
+ *@format: pointer to format specifier
+ *Return: char_count, the number of character printed
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list args;
+
+	check_t check[] = {
+		{"c", print_str},
+		{"s", print_str},
+		{NULL, NULL}
+};
+
+	int i;
+	int j;
+	int char_count;
+
+	va_start(args, format);
+	if (format == NULL)
+	{
+		return (-1);
+		exit;
+	}
+
+	i = 0;
+	char_count = 0;
+
+	while (format[i] != '\0')
+	{
+		if (format[i] == '%')
+		{
+			char_count = char_count + _strlen(args[i]);
+			format[i + 1];
+		}
+
+		j = 0;
+		while (check[j] != NULL)
+		{
+			if (*check[j].op == format[i + 1])
+			{
+				(check[j].f)(args);
+			}
+			j = j + 1;
+			i = i + 2;
+		}
+		_putchar(format[i]);
+		i = i + 1;
+		char_count = char_count + 1;
+	}
+	return (char_count);
+}
